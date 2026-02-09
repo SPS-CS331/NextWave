b699dbf883f1d3c9ccd8dad70bf26e55df692612
Diya-16204 <151431840+Diya-16204@users.noreply.github.com>
2026-02-10 02:35:27 +0530

    Update User.js

--- Forensics/backend/models/User.js
+++ Forensics/backend/models/User.js
@@ -1,1 +1,17 @@
 
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  role: String,
+  adminId: String,
+  investigatorId: String,
+  analystId: String,
+  department: String,
+  specialization: String,
+  googleId: String,
+});
+
+module.exports = mongoose.model("User", userSchema);
